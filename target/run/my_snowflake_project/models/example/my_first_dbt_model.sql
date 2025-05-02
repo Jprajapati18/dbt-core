097db640c4d2de5694bb20c9ea3f020f8e3092cc
jprajapati18 <jigar1801@outlook.com>
2025-05-02 17:14:50 +0530

    Initial DBT project setup

---
+++ target/run/my_snowflake_project/models/example/my_first_dbt_model.sql
@@ -0,0 +1,26 @@
+
+  
+    
+
+        create or replace transient table PC_DBT_DB.SNOWFLAKE_SAMPLE_DATA.my_first_dbt_model
+         as
+        (/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+
+
+select * from SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.NATION
+
+/*
+    Uncomment the line below to remove records with null `id` values
+*/
+
+-- where id is not null
+        );
+      
+  
